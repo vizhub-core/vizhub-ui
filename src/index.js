ff5493ca0dfa9fe63b99c231fec035ef0c2bc102
Curran <curran.kelleher@gmail.com>
2023-03-25 03:19:15 -0400

    Instantiate the latest vision for what this should be

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { DemoApp } from './DemoApp';
-
-export const demoApp = () => {
-  ReactDOM.render(<DemoApp />, document.getElementById('root'));
-};
